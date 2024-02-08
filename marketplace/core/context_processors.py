8f83bc2b1e4270ff1ecb3eac17e4f1d4aa7a627a
Ali <99434560+Ali-tnt@users.noreply.github.com>
2024-02-08 22:39:00 +0330

    Delete marketplace directory
    commit c333d1f is correct, this and last one are a mess and i'm cleaning it

--- marketplace/core/context_processors.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import messages
-
-def js_messages(request):
-    js_messages = [str(message) for message in messages.get_messages(request)]
-    return {'js_messages': js_messages}
-#TODO اینجا یه مقدار بفرستم با جی اس چکش کنم، اگر یوزر خارج شد مقدارشو عوض کنم جی اس بفهمه بعد باز با جی اس مقدارشو برگردونم
--- marketplace/marketplace/dashboard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-app_name = 'dashboard'
-
-urlpatterns = [
-    path('', views.index , name='index'),
-    path('admin_dash', views.admin_dash , name='admin_dash'),
-]
